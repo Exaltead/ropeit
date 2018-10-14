0eb2167562ea5083f0dc94a82802220a9bf87212
Aku <aku.pohjola@student.tut.fi>
2018-10-14 15:36:52 +0300

    Base for character details is added.

--- client/src/components/CharacterDetailsRead.tsx
+++ client/src/components/CharacterDetailsRead.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
 import { Character as CharacterModel } from 'src/store/characters/types'
+import styled from 'styled-components';
 
+
+const BackDiv = styled.div`
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    margin: 1rem;
+    padding: 1rem;
+    height: 90%
+`
 
 
 interface Props {
@@ -8,9 +16,18 @@ interface Props {
 }
 
 const CharacterDetailsRead = (props: Props) => {
-    console.log(props)
+    const character = props.character
     return (
-        <p>Details of character {props.character.id}</p>
+        <BackDiv>
+            <h1>{character.name}</h1>
+            <h2>Mechanics</h2>
+            <p>{character.mechanics}</p>
+            <h2>Notes</h2>
+            <p>{character.notes}</p>
+            <h2>Fluff</h2>
+            <p>{character.fluff}</p>
+
+        </BackDiv>
     )
 }
 
